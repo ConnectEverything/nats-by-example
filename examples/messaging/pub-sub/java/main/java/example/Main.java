46e1e98f31e7c4bd4e13ed8c81aa88bab5682cad
Byron Ruth <b@devel.io>
2022-07-26 07:08:05 -0400

    Fix import
    Signed-off-by: Byron Ruth <b@devel.io>

--- examples/messaging/pub-sub/java/main/java/example/Main.java
+++ examples/messaging/pub-sub/java/main/java/example/Main.java
@@ -1,6 +1,8 @@
 package example;
 
-import io.nats.*;
+import io.nats.client.{Connection,Nats};
+
+import java.nio.charset.StandardCharsets;
 
 public class Main {
   public static void main(String[] args) {
@@ -12,13 +14,13 @@ public class Main {
     // Initialize a connection to the server.
     Connection nc = Nats.connect(natsURL);
 
+    // Prepare a simple message body.
     Bytes messageBytes = "hello".getBytes(StandardCharsets.UTF_8);
 
-    // Publish a message.
+    // Publish the message.
     nc.publish("greet.joe", messageBytes);
 
-    // Create a message dispatcher for handling messages in a separate
-    // thread.
+    // Create a message dispatcher for handling messages in a separate thread.
     Dispatcher dispatcher = nc.createDispatcher((msg) -> {
       System.out.printf("%s on subject %s\n",
           new String(msg.getData(), StandardCharsets.UTF_8),
@@ -29,6 +31,7 @@ public class Main {
     dispatcher.subscribe("greet.*");
 
     // Publish more messages that will be received by the subscription.
+    // Note the first message on `greet.joe` was not received.
     nc.publish("greet.bob", messageBytes);
     nc.publish("greet.sue", messageBytes);
     nc.publish("greet.pam", messageBytes);
