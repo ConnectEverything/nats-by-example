38dae4137e3c416e666f0c54a752a779793b266c
Byron Ruth <b@devel.io>
2022-08-01 14:18:56 -0400

    Demonstrate custom system account
    Signed-off-by: Byron Ruth <b@devel.io>

--- examples/auth/sys-account/cli/main.sh
+++ examples/auth/sys-account/cli/main.sh
@@ -38,10 +38,27 @@ nats context save sys \
   --user sys \
   --password pass
 
-
 # Now we can try getting the server info again. We need to set the
 # user and password.
 nats --context sys server info
 
+# There is another option to change the default system account name which
+# is required when using the decentralized JWT auth method. However, we will
+# demonstrate here. We can define a new account and then set the
+# `system_account` option to the account to use.
+cat <<- EOF > server.conf
+accounts: {
+  MYSYS: {
+    users: [{user: sys, password: pass}]
+  }
+}
+
+system_account: MYSYS
+EOF
+
+# We will reload again and then run the command again with out custom
+# account system account.
+nats-server --signal reload=$SERVER_PID
+nats --context sys server info
 
 kill $SERVER_PID
