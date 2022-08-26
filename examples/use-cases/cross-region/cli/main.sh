43b6ed6cd38f5829353260705725c9a463c4b365
Byron Ruth <b@devel.io>
2022-08-26 07:40:58 -0400

    Clean up comments and tags
    Signed-off-by: Byron Ruth <b@devel.io>

--- examples/use-cases/cross-region/cli/main.sh
+++ examples/use-cases/cross-region/cli/main.sh
@@ -2,10 +2,9 @@
 
 set -euo pipefail
 
-# Define a shared configuration which enables JetStream
-# and defines the `unique_tag` option which enforces all
-# replicas of a stream or consumer must be in different
-# availability zones.
+# Create a shared configuration which enables JetStream and defines the
+# `unique_tag` option which enforces all replicas for a given stream or
+# consumer to be placed on nodes with different availability zones (AZ).
 cat <<- EOF > shared.conf
 jetstream: {
   unique_tag: "az:"
@@ -22,7 +21,7 @@ accounts: {
 }
 EOF
 
-# Define the shared clustered config defining the seed routes.
+# Create the shared clustered config defining the seed routes.
 cat <<- EOF > cluster.conf
 name: c1
 routes: [
@@ -33,17 +32,21 @@ routes: [
 EOF
 
 # Define nine server configurations modeling a cluster with three nodes
-# in each region across three availability zones (AZs). NATS does not
-# currently support declaring tags with logical OR, so valid
-# combinations can be precomputed as tags and then used when creating
-# streams. In this case, the `xr:` tag encodes the cluster/AZ combination.
+# in each region across three AZs. NATS does not currently support
+# declaring tags with logical OR (or exclusive OR), so valid combinations
+# can be precomputed as tags and then used when creating streams. In this
+# case, the `xr:` tag encodes the cluster/AZ combination while still adhering
+# to the `unique_tag` requirement.
 # Each index corresponds to a region (e.g us-east) and the value at the
 # index corresponds to an AZ, e.g. us-east4.
-# A tag `xr:231` can be read as a cross-region stream where the replica
-# in region 1 is AZ 2, region 2 is AZ 3and region 3 is AZ 1.
+# Thus a tag `xr:231` can be read as a cross-region stream where one replica
+# is placed in region 1 in AZ 2, another in region 2 in AZ 3, and the third
+# replica in region 3 in AZ 1.
+# In addition to the `xr:` tags, the standard `rg:` (region) and `az:` (AZ)
+# tags are set to support _regional_ streams which is demonstrated below. 
 cat <<- EOF > "rg1-az1.conf"
 server_name: rg1-az1
-server_tags: [rg:1, az:1, xr:111, xr:112, xr:113, xr:121, xr:122, xr:123, xr:131, xr:132, xr:133]
+server_tags: [rg:1, az:1, xr:123, xr:132]
 port: 4222
 include shared.conf
 cluster: {
@@ -54,7 +57,7 @@ EOF
 
 cat <<- EOF > "rg1-az2.conf"
 server_name: rg1-az2
-server_tags: [rg:1, az:2, xr:211, xr:212, xr:213, xr:221, xr:222, xr:223, xr:231, xr:232, xr:233]
+server_tags: [rg:1, az:2, xr:213, xr:231]
 port: 4223
 include shared.conf
 cluster: {
@@ -65,7 +68,7 @@ EOF
 
 cat <<- EOF > "rg1-az3.conf"
 server_name: rg1-az3
-server_tags: [rg:1, az:3, xr:311, xr:312, xr:313, xr:321, xr:322, xr:323, xr:331, xr:332, xr:333]
+server_tags: [rg:1, az:3, xr:312, xr:321]
 port: 4224
 include shared.conf
 cluster: {
@@ -76,7 +79,7 @@ EOF
 
 cat <<- EOF > "rg2-az1.conf"
 server_name: rg2-az1
-server_tags: [rg:2, az:1, xr:111, xr:112, xr:113, xr:211, xr:212, xr:213, xr:311, xr:312, xr:313]
+server_tags: [rg:2, az:1, xr:213, xr:312]
 port: 4225
 include shared.conf
 cluster: {
@@ -87,7 +90,7 @@ EOF
 
 cat <<- EOF > "rg2-az2.conf"
 server_name: rg2-az2
-server_tags: [rg:2, az:2, xr:121, xr:122, xr:123, xr:221, xr:222, xr:223, xr:321, xr:322, xr:323]
+server_tags: [rg:2, az:2, xr:123, xr:321]
 port: 4226
 include shared.conf
 cluster: {
@@ -98,7 +101,7 @@ EOF
 
 cat <<- EOF > "rg2-az3.conf"
 server_name: rg2-az3
-server_tags: [rg:2, az:3, xr:131, xr:132, xr:133, xr:231, xr:232, xr:233, xr:331, xr:332, xr:333]
+server_tags: [rg:2, az:3, xr:132, xr:231]
 port: 4227
 include shared.conf
 cluster: {
@@ -109,7 +112,7 @@ EOF
 
 cat <<- EOF > "rg3-az1.conf"
 server_name: rg3-az1
-server_tags: [rg:3, az:1, xr:111, xr:211, xr:311, xr:221, xr:231, xr:321, xr:331]
+server_tags: [rg:3, az:1, xr:231, xr:321]
 port: 4228
 include shared.conf
 cluster: {
@@ -120,7 +123,7 @@ EOF
 
 cat <<- EOF > "rg3-az2.conf"
 server_name: rg3-az2
-server_tags: [rg:3, az:2, xr:112, xr:113, xr:211, xr:221, xr:231, xr:311, xr:321, xr:331]
+server_tags: [rg:3, az:2, xr:132, xr:312]
 port: 4229
 include shared.conf
 cluster: {
@@ -131,7 +134,7 @@ EOF
 
 cat <<- EOF > "rg3-az3.conf"
 server_name: rg3-az3
-server_tags: [rg:3, az:3, xr:113, xr:213, xr:313, xr:223, xr:233, xr:313, xr:323, xr:333]
+server_tags: [rg:3, az:3, xr:123, xr:213]
 port: 4230
 include shared.conf
 cluster: {
@@ -147,14 +150,14 @@ for c in $(ls rg*.conf); do
   sleep 1
 done
 
-# Show a few reports to ensure the nodes are up and JetStream is healthy.
-nats --user sys --password sys server info
+# Show the server lit and JetStream report to ensure the nodes are up
+# and JetStream is healthy.
 nats --user sys --password sys server list
 nats --user sys --password sys server report jetstream
 
 # Create a cross-region stream using one of the pre-defined `xr:` tags.
 nats --user user --password user stream add \
-  --tag=xr:113 \
+  --tag=xr:123 \
   --retention=limits \
   --storage=memory \
   --replicas=3 \
@@ -196,3 +199,5 @@ nats --user user --password user stream add \
 
 # Report out the streams.
 nats --user user --password user stream report
+
+
