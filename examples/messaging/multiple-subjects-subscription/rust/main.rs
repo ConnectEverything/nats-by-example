349b91f221ac33a02e26332d8c6f779d0f85ba6c
Jarema <tomasz@nats.io>
2022-08-28 13:31:09 +0200

    Improve example

--- examples/messaging/multiple-subjects-subscription/rust/main.rs
+++ examples/messaging/multiple-subjects-subscription/rust/main.rs
@@ -11,20 +11,32 @@ async fn main() -> Result<(), async_nats::Error> {
     let client = async_nats::connect(nats_url).await?;
 
     // Core NATS `client.subscribe()` can subscribe to more than one explicit subject using
-    // `>` and `*` wildcards, but not to two distinct subjects.
-    // However, it's still possible to have just one stream of messages for such case.
+    // `>` and `*` wildcards, but not to two or more distinct subjects.
+    // However, it's still possible to have just one stream of messages for such use case.
     //
-    // First, let's subscribe to those subjects.
+    // First, let's subscribe to desired subjects.
     let subscription_cars = client.subscribe("cars.>".into()).await?;
     let subscription_planes = client.subscribe("planes.>".into()).await?;
+    let subscription_ships = client.subscribe("ships.>".into()).await?;
 
-    // Then, spawn two tasks that publishes to both subject at the same time.
+    // Then, spawn three tasks that publishes to each subject at the same time.
     tokio::task::spawn({
         let client = client.clone();
         async move {
             for i in 0..100 {
                 client
                     .publish(format!("cars.{}", i), format!("car number {}", i).into())
+                    .await?;
+            }
+            Ok::<(), async_nats::Error>(())
+        }
+    });
+    tokio::task::spawn({
+        let client = client.clone();
+        async move {
+            for i in 0..100 {
+                client
+                    .publish(format!("ships.{}", i), format!("ship number {}", i).into())
                     .await?;
             }
             Ok::<(), async_nats::Error>(())
@@ -45,10 +57,14 @@ async fn main() -> Result<(), async_nats::Error> {
         }
     });
 
-    // Now, let's create the cars stream that will receive messages from both subscriptions.
-    // If you need more than two subscriptions, consider using `futures::stream::select_all`.
+    // Now, let's create the stream that will receive messages from all subscriptions.
     // We also leverage the `take()` combinator to limit number of messages we want to get in total.
-    let mut messages = futures::stream::select(subscription_cars, subscription_planes).take(200);
+    //
+    // If you need to get messages from at most two subscriptions, `futures::stream::select` can be
+    // used instead.
+    let mut messages =
+        futures::stream::select_all([subscription_cars, subscription_planes, subscription_ships])
+            .take(300);
 
     // Receive the messages from both subjects.
     while let Some(message) = messages.next().await {
