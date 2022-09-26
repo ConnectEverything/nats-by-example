e737ad7d34e44fff2e9c8f1e8fd46b654222ac87
Byron Ruth <b@devel.io>
2022-09-26 17:20:00 -0400

    Split into two examples
    Signed-off-by: Byron Ruth <b@devel.io>

--- examples/topologies/supercluster-jetstream/cli/main.sh
+++ examples/topologies/supercluster-jetstream/cli/main.sh
@@ -13,15 +13,17 @@ accounts: {
 system_account: SYS
 EOF
 
-# Create the *east* and *west* server configurations.
-# A requirement of JetStream is to have a cluster block defined
-# since this is how available storage resources are determined
-# for placement of streams and consumers.
+# Create the *east*, *central*, and *west* server configurations.
+# A requirement of JetStream is to have a cluster block with routes
+# defined, even for single node clusters.
 #
 # In a production deployment, at least three nodes per cluster
 # are recommended which supports creating R3 streams. In this
 # test setup, only R1 streams can be created since streams replicas
 # do not cross gateway connections.
+#
+# Similarity, across a supercluster, an odd number of servers must
+# exist to support reaching consensus for the meta group leader.
 cat <<- EOF > east.conf
 port: 4222
 http_port: 8222
@@ -44,12 +46,13 @@ gateway: {
   port: 7222,
   gateways: [
     {name: "east", urls: ["nats://0.0.0.0:7222"]},
-    {name: "west", urls: ["nats://0.0.0.0:7223"]},
+    {name: "central", urls: ["nats://0.0.0.0:7223"]},
+    {name: "west", urls: ["nats://0.0.0.0:7224"]},
   ]
 }
 EOF
 
-cat <<- EOF > west.conf
+cat <<- EOF > central.conf
 port: 4223
 http_port: 8223
 server_name: n2
@@ -59,7 +62,7 @@ include sys.conf
 jetstream: {}
 
 cluster: {
-  name: west,
+  name: central,
   port: 6223,
   routes: [
     "nats-route://0.0.0.0:6223"
@@ -67,11 +70,41 @@ cluster: {
 }
 
 gateway: {
-  name: "west",
+  name: "central",
   port: 7223,
   gateways: [
     {name: "east", urls: ["nats://0.0.0.0:7222"]},
-    {name: "west", urls: ["nats://0.0.0.0:7223"]},
+    {name: "central", urls: ["nats://0.0.0.0:7223"]},
+    {name: "west", urls: ["nats://0.0.0.0:7224"]},
+  ]
+}
+EOF
+
+
+cat <<- EOF > west.conf
+port: 4224
+http_port: 8224
+server_name: n3
+
+include sys.conf
+
+jetstream: {}
+
+cluster: {
+  name: west,
+  port: 6224,
+  routes: [
+    "nats-route://0.0.0.0:6224"
+  ],
+}
+
+gateway: {
+  name: "west",
+  port: 7224,
+  gateways: [
+    {name: "east", urls: ["nats://0.0.0.0:7222"]},
+    {name: "central", urls: ["nats://0.0.0.0:7223"]},
+    {name: "west", urls: ["nats://0.0.0.0:7224"]},
   ]
 }
 EOF
@@ -79,6 +112,7 @@ EOF
 
 # Start the servers and sleep for a few seconds to startup.
 nats-server -c east.conf > /dev/null 2>&1 &
+nats-server -c central.conf > /dev/null 2>&1 &
 nats-server -c west.conf > /dev/null 2>&1 &
 
 sleep 3
@@ -95,18 +129,23 @@ curl --fail --silent \
   --retry-delay 1 \
   http://localhost:8223/healthz > /dev/null
 
+curl --fail --silent \
+  --retry 5 \
+  --retry-delay 1 \
+  http://localhost:8224/healthz > /dev/null
+
 
 # Save a couple NATS CLI contexts for convenience.
 nats context save east \
-  --server "nats://localhost:4222"
+  --server "nats://localhost:4222" > /dev/null
 
 nats context save east-sys \
   --server "nats://localhost:4222" \
   --user sys \
-  --password pass
+  --password pass > /dev/null
 
 nats context save west \
-  --server "nats://localhost:4223"
+  --server "nats://localhost:4223" > /dev/null
 
 
 # Show the server list which will indicate the clusters and
