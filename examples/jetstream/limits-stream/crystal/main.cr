2e68c3d768ee05ab6af233468a684e1a96d66006
Jamie Gaskins <jgaskins@hey.com>
2022-12-22 18:36:53 -0500

    Add documentation

--- examples/jetstream/limits-stream/crystal/main.cr
+++ examples/jetstream/limits-stream/crystal/main.cr
@@ -1,6 +1,5 @@
 require "nats"
 require "nats/jetstream"
-require "log"
 
 # Get the `NATS_URL` from the environment or fallback to the default. This can
 # be a comma-separated string. We convert it to an `Array(URI)` to pass to the
@@ -18,6 +17,10 @@ at_exit { nats.close }
 
 js = nats.jetstream
 
+# Here we create the `EVENTS` stream that listens on all subjects matching
+# `events.>` (all subjects starting with `events.`), stored on the filesystem
+# for durability, and can contain up to 10 messages. The default discard policy
+# will discard old messages when we exceed that limit.
 stream = js.stream.create(
   name: "EVENTS",
   subjects: %w[events.>],
@@ -25,6 +28,8 @@ stream = js.stream.create(
   max_msgs: 10_i64,
 )
 
+# We publish 11 messages to our 10-message stream, so we will exceed our limit.
+# As mentioned above, this will discard older messages when new ones come in.
 event_types = %w[
   page_loaded
   mouse_clicked
@@ -33,13 +38,17 @@ event_types = %w[
 11.times do
   type = event_types.sample
   subject = "events.#{type}"
-  Log.info { "Publishing #{subject}..." }
+  puts "Publishing #{subject}..."
   js.publish(subject, "")
 end
 
+# When we fetch the current stream state from the server, we see that there are
+# indeed only 10 messages in the stream, with sequence numbers 2..11. This
+# indicates that the first message in our stream with a capacity of 10 was
+# dropped to make room for the 11th message.
 name = stream.config.name
 if stream = js.stream.info(name)
-  Log.info { stream.state }
+  pp stream.state
 else
   raise "Could not find stream: #{name}"
 end
