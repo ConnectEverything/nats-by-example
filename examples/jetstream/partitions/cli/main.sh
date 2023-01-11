137d8bb94da702e3a36c2167e50dfb97e1e0af8f
Byron Ruth <byron@nats.io>
2023-01-11 08:40:43 -0500

    Update comments
    Signed-off-by: Byron Ruth <byron@nats.io>

--- examples/jetstream/partitions/cli/main.sh
+++ examples/jetstream/partitions/cli/main.sh
@@ -4,12 +4,22 @@ set -euo pipefail
 
 unset NATS_URL
 
-# Define the system account to be included by all configurations.
+# Define a shared config included in each of the individual
+# node configs.
+# Notice the `mappings` option defined on the `APP` account
+# which takes a published message such as `events.1` and
+# will map it to `events.1.4`, where the last token indicate
+# the deterministic partition number. In this case, the second
+# token `1` is mapped to a partition between 0 and 4 (five total
+# partitions).
+# *Note: you can have more than one token when defining the
+# partitioning.*
 cat <<- EOF > shared.conf
 accounts: {
   SYS: {
     users: [{user: sys, password: sys}]
   }
+
   APP: {
     jetstream: true,
     users: [{user: app, password: app}]
@@ -22,15 +32,10 @@ accounts: {
 system_account: SYS
 EOF
 
-# Create the *east* and *west* server configurations.
-# A requirement of JetStream is to have a cluster block defined
-# since this is how available storage resources are determined
-# for placement of streams and consumers.
-#
-# In a production deployment, at least three nodes per cluster
-# are recommended which supports creating R3 streams. In this
-# test setup, only R1 streams can be created since streams replicas
-# do not cross gateway connections.
+# Declare the configuration per node including the shared
+# configuration. Note, if decentralized auth is being used
+# mappings can be defined via the `nsc` tool on account
+# JWTs.
 cat <<- EOF > n1.conf
 port: 4222
 http_port: 8222
@@ -98,15 +103,14 @@ cluster: {
 EOF
 
 
-# Start the servers and sleep for a few seconds to startup.
+# Start the cluster and wait a few seconds to choose the leader.
 nats-server -c n1.conf > /dev/null 2>&1 &
 nats-server -c n2.conf > /dev/null 2>&1 &
 nats-server -c n3.conf > /dev/null 2>&1 &
 
 sleep 3
 
-
-# Save and select the default context to use all seed servers.
+# Save and select the default context for convenience.
 nats context save \
   --server "nats://localhost:4222,nats://localhost:4223,nats://localhost:4224" \
   --user app \
@@ -116,6 +120,8 @@ nats context save \
 nats context select default
 
 # Create five streams modeling partitions.
+# Note the `--subjects` option correponds to the subject mapping
+# we defined above, `events.*.0`, `events.*.1`, etc.
 for i in $(seq 0 4); do
   nats stream add \
     --retention=limits \
@@ -136,8 +142,14 @@ for i in $(seq 0 4); do
     "events-$i" > /dev/null
 done
 
+# Report the stream to confirm they are present.
 nats stream report
 
+# Run a benchmark of one publisher using synchronous acks.
+# The `--multisubject` option the sequence number as a token
+# to the base subject `events` in this case. The `--stream`
+# option is simply an override to using the default stream
+# but does not impact behavior.
 nats bench \
   --js \
   --multisubject \
@@ -148,8 +160,14 @@ nats bench \
   --stream "events-0" \
   "events"
 
+# As expected the throughput is quite low due to the synchronous
+# publish.
+# However, looking at the stream report again, we see the messages
+# have been distributed to each stream.
 nats stream report
 
+# Let's purge the streams and try the benchmark again with
+# a async batch of 100.
 for i in $(seq 0 4); do
   nats stream purge -f "events-$i" > /dev/null
 done
@@ -164,8 +182,11 @@ nats bench \
   --stream "events-0" \
   "events"
 
+# As expected the throughput increases.
 nats stream report
 
+# Purging one more time, we will also increase the number of
+# concurrent publishers to 10.
 for i in $(seq 0 4); do
   nats stream purge -f "events-$i" > /dev/null
 done
@@ -180,4 +201,15 @@ nats bench \
   --stream "events-0" \
   "events"
 
+# The throughput increases a bit more. Do note, that throughput
+# and latency will highly depend on the quality of the network,
+# and the resources available on the servers hosing the streams.
+#
+# *Note: If you are looking at the output on NATS by Example and are
+# unimpressed with the numbers, do know this example ran in a
+# GitHub Actions runner which fairly low-resourced having 2 cpu
+# cores and 7GB of memory. Given this examples sets up three
+# servers with five streams all of which are replicated as well
+# as running the CLI for benchmarking (which takes up resources),
+# true performance will suffer in comparison to a production setup.*
 nats stream report
