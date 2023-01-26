d5a40bfb7b82a4fb4b73a4a259191666e7563388
Byron Ruth <byron@nats.io>
2023-01-26 08:59:47 -0500

    Address comments
    Signed-off-by: Byron Ruth <byron@nats.io>

--- examples/jetstream/workqueue-stream/java/Main.java
+++ examples/jetstream/workqueue-stream/java/Main.java
@@ -8,7 +8,6 @@ import io.nats.client.api.StreamState;
 import io.nats.client.api.RetentionPolicy;
 import io.nats.client.api.AckPolicy;
 
-import java.io.IOException;
 import java.time.Duration;
 import java.nio.charset.StandardCharsets;
 
@@ -36,26 +35,28 @@ public class Main {
         .build();
 
       jsm.addStream(sc);
-      System.out.printf("Created stream %s\n", sc.getName());
+
+      StreamInfo si = jsm.addStream(sc);
+      System.out.printf("Created stream %s\n\n", si.getConfiguration().getName());
 
       // ### Queue
       // Create a `JetStream` context for publishing and subscribing.
       JetStream js = nc.jetStream();
 
-      js.publish("events.us.page_loaded", null);
-      js.publish("events.eu.mouse_clicked", null);
-      js.publish("events.us.input_focused", null);
+      js.publish("events.us.page_loaded", "time_ms: 120".getBytes(StandardCharsets.UTF_8));
+      js.publish("events.eu.mouse_clicked", "x: 510, y: 1039".getBytes(StandardCharsets.UTF_8));
+      js.publish("events.us.input_focused", "id: username".getBytes(StandardCharsets.UTF_8));
       System.out.println("Published 3 messages");
 
       StreamInfo info = jsm.getStreamInfo("EVENTS");
       long msgCount = info.getStreamState().getMsgCount();
-      System.out.printf("msgs in stream: %s\n", msgCount);
+      System.out.printf("msgs in stream: %s\n\n", msgCount);
 
       // ### Adding a consumer
       // Now let's add a consumer. It can be a *push* or *pull* consumer,
       // however for this example we will use *pull*.
       // The first pull consumer will be bound to all subjects in
-      // the stream. 
+      // the stream, since we did not specify `filterSubject(...)`.
       ConsumerConfiguration c1 = ConsumerConfiguration.builder()
         .durable("processor-1")
         .ackPolicy(AckPolicy.Explicit)
@@ -63,15 +64,23 @@ public class Main {
 
       jsm.addOrUpdateConsumer("EVENTS", c1);
 
-      // Now we bind the subscription. Note the empty string for the subject
-      // implies interest in all subjects in the stream.
+      // Now we bind the subscription. Note that null or empty string
+      // for the subject tells the subscription to use exactly the
+      // filter subject provided by the consumer.
       PullSubscribeOptions o1 = PullSubscribeOptions.bind("EVENTS", "processor-1");
-      JetStreamSubscription s1 = js.subscribe("", o1);
+      JetStreamSubscription s1 = js.subscribe(null, o1);
 
-      // Now we can fetch all three messages in the stream.
-      s1.pull(3);
-      for (int i = 0; i < 3; i++) {
+      // Now we can get all three messages in the stream. Note, we
+      // are requesting more messages than we know exist in the stream
+      // simply to demonstrate that the pull consumer will only get
+      // the messages that are available and return null when there
+      // are no more messages.
+      s1.pull(5);
+      for (int i = 0; i < 5; i++) {
         Message msg = s1.nextMessage(Duration.ofSeconds(1));
+        if (msg == null) {
+          break;
+        }
         msg.ack();
         System.out.printf("Received event: %s\n", msg.getSubject());
       }
@@ -80,12 +89,12 @@ public class Main {
       // We can confirm by checking the stream state again.
       info = jsm.getStreamInfo("EVENTS");
       msgCount = info.getStreamState().getMsgCount();
-      System.out.printf("msgs in stream: %s\n", msgCount);
+      System.out.printf("msgs in stream: %s\n\n", msgCount);
 
       // ### Exclusive non-filtered consumer
       // As noted in the description above, work-queue streams can only
       // have at most one consumer with interest on a subject at any given
-      // time. Since this first pull consumer is not filtered, it we try
+      // time. Since this first pull consumer is not filtered, if we try
       // to create another consumer, it will fail.
       ConsumerConfiguration c2 = ConsumerConfiguration.builder()
         .durable("processor-2")
@@ -95,17 +104,18 @@ public class Main {
       try {
         jsm.addOrUpdateConsumer("EVENTS", c2);
       } catch (JetStreamApiException e) {
-        System.out.println("Failed to create an overlapping consumer");
+        System.out.printf("Failed to create an overlapping consumer:\n%s\n\n", e.getMessage());
       }
 
-      // However if we delete the first consumer, this will clean up
-      // the first consumer since it was an ephemeral, which removes interest.
+      // However if we delete the first consumer, this will remove
+      // the interest and allow us to create a new consumer.
       jsm.deleteConsumer("EVENTS", "processor-1");
       System.out.println("Deleting the first consumer");
 
       jsm.addOrUpdateConsumer("EVENTS", c2);
       System.out.println("Succeeded to create a new consumer");
       jsm.deleteConsumer("EVENTS", "processor-2");
+      System.out.println("Deleting the new consumer\n");
 
       // ### Multiple filtered consumers
       // To create multiple consumers, we need to filter the subjects.
@@ -122,7 +132,7 @@ public class Main {
 
       PullSubscribeOptions o3 = PullSubscribeOptions.bind("EVENTS", "processor-3");
 
-      JetStreamSubscription s3 = js.subscribe("", o3);
+      JetStreamSubscription s3 = js.subscribe(null, o3);
 
       ConsumerConfiguration c4 = ConsumerConfiguration.builder()
         .durable("processor-4")
@@ -134,12 +144,12 @@ public class Main {
 
       PullSubscribeOptions o4 = PullSubscribeOptions.bind("EVENTS", "processor-4");
 
-      JetStreamSubscription s4 = js.subscribe("", o4);
+      JetStreamSubscription s4 = js.subscribe(null, o4);
       System.out.println("Created two filtered consumers");
 
-      js.publish("events.us.page_loaded", null);
-      js.publish("events.eu.mouse_clicked", null);
-      js.publish("events.us.input_focused", null);
+      js.publish("events.us.page_loaded", "time_ms: 103".getBytes(StandardCharsets.UTF_8));
+      js.publish("events.eu.mouse_clicked", "x: 1040, y: 39".getBytes(StandardCharsets.UTF_8));
+      js.publish("events.us.input_focused", "id: password".getBytes(StandardCharsets.UTF_8));
       System.out.println("Published 3 more messages");
 
       s3.pull(2);
@@ -159,10 +169,6 @@ public class Main {
 
       s4.unsubscribe();
       jsm.deleteConsumer("EVENTS", "processor-4");
-
-      // Sleep this thread a little so the dispatcher thread has time
-      // to receive all the messages before the program quits.
-      Thread.sleep(200);
 
     } catch (Exception e) {
       e.printStackTrace();
