cfeef8c7d0cdfe8a4ae5ccff1e1f0e5d45820a7a
Byron Ruth <byron@nats.io>
2023-01-26 09:57:06 -0500

    Add sleep to ensure Debezium is ready
    Signed-off-by: Byron Ruth <byron@nats.io>

--- examples/integrations/debezium/cli/main.sh
+++ examples/integrations/debezium/cli/main.sh
@@ -7,6 +7,9 @@ until nc -z nats 4222; do sleep 1; done
 until nc -z postgres 5432; do sleep 1; done
 until nc -z debezium 8080; do sleep 1; done
 
+# Allow Debezium to setup its connection to Postgres.
+sleep 1
+
 # Create a table and insert, update, and delete some data.
 printf 'Create and populate the database.\n'
 psql -h postgres -c "create table profile (id serial primary key, name text, color text);"
@@ -28,9 +31,9 @@ sleep 1
 
 # Print the stream subjects, seeing each schema/table pair as subjects.
 printf '\nStream subjects.\n'
-nats stream subjects --names DebeziumStream
+nats stream subjects DebeziumStream
 
 # Print the data in the stream, plucking out the before and after values
 # for ease of reading.
 printf '\nChange events.\n'
-nats stream view --raw DebeziumStream 2> /dev/null | jq -r '{before: .payload.before, after: .payload.after}'
+nats stream view --raw DebeziumStream 2> /dev/null | jq -r '.payload'
