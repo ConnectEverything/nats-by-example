309dcb4c3a6a3dcdc00a250211789aa6a16c423f
Byron Ruth <byron@nats.io>
2023-01-26 08:40:25 -0500

    Update description and output
    Signed-off-by: Byron Ruth <byron@nats.io>

--- examples/use-cases/debezium/cli/main.sh
+++ examples/use-cases/debezium/cli/main.sh
@@ -8,18 +8,29 @@ until nc -z postgres 5432; do sleep 1; done
 until nc -z debezium 8080; do sleep 1; done
 
 # Create a table and insert, update, and delete some data.
-psql -h postgres -c "create table test (id serial primary key, name text, color text);"
+printf 'Create and populate the database.\n'
+psql -h postgres -c "create table profile (id serial primary key, name text, color text);"
 
-psql -h postgres -c "insert into test (name, color) values ('joe', 'blue');"
-psql -h postgres -c "insert into test (name, color) values ('pam', 'green');"
+psql -h postgres -c "insert into profile (name, color) values ('Joe', 'blue');"
+psql -h postgres -c "insert into profile (name, color) values ('Pam', 'green');"
 
-psql -h postgres -c "update test set color = 'red' where name = 'joe';"
-psql -h postgres -c "update test set color = 'yellow' where name = 'pam';"
+psql -h postgres -c "update profile set color = 'red' where name = 'Joe';"
+psql -h postgres -c "update profile set color = 'yellow' where name = 'Pam';"
 
-psql -h postgres -c "delete from test where name = 'joe';"
+psql -h postgres -c "delete from profile where name = 'Joe';"
 
-# Ensure the data is in the stream.
+psql -h postgres -c "create table books (id serial primary key, title text, author text);"
+
+psql -h postgres -c "insert into books (title, author) values ('NATS Diaries', 'Pam');"
+
+# Ensure the change events have been published to the stream.
 sleep 1
 
-# Print the data in the stream.
-nats stream view --raw DebeziumStream | jq '{before: .payload.before, after: .payload.after}'
+# Print the stream subjects, seeing each schema/table pair as subjects.
+printf '\nStream subjects.\n'
+nats stream subjects --names DebeziumStream
+
+# Print the data in the stream, plucking out the before and after values
+# for ease of reading.
+printf '\nChange events.\n'
+nats stream view --raw DebeziumStream 2> /dev/null | jq -r '{before: .payload.before, after: .payload.after}'
