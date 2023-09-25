c3a1a66a5bf25193f466bd6b50fb62f7703c5d38
Byron Ruth <byron@nats.io>
2023-09-25 09:53:02 -0400

    Add centralized auth-callout example (#134)
    Signed-off-by: Byron Ruth <byron@nats.io>

---
+++ examples/auth/callout/cli/main.sh
@@ -0,0 +1,104 @@
+#!/bin/bash
+
+set -eou pipefail
+
+NATS_URL="nats://localhost:4222"
+
+# Use a static issuer Nkey and Xkey for this example.
+# Typically this would be generated using the `nsc generate nkey --account`
+# and `nsc generate nkey --curve` commands, respectively, or programmatically
+# with one of the `nkeys` libraries.
+ISSUER_NKEY="ABJHLOVMPA4CI6R5KLNGOB4GSLNIY7IOUPAJC4YFNDLQVIOBYQGUWVLA"
+ISSUER_NSEED="SAANDLKMXL6CUS3CP52WIXBEDN6YJ545GDKC65U5JZPPV6WH6ESWUA6YAI"
+
+ISSUER_XKEY="XAB3NANV3M6N7AHSQP2U5FRWKKUT7EG2ZXXABV4XVXYQRJGM4S2CZGHT"
+ISSUER_XSEED="SXAAXMRAEP6JWWHNB6IKFL554IE6LZVT6EY5MBRICPILTLOPHAG73I3YX4"
+
+# Create multi-account config with auth callout enabled and encryption.
+cat <<- EOF > server.conf
+accounts {
+  AUTH {
+    users: [
+      { user: auth, password: auth }
+    ]
+  }
+  APP {}
+  SYS {}
+}
+
+authorization {
+  auth_callout {
+    issuer: $ISSUER_NKEY
+    users: [ auth ]
+    account: AUTH
+    xkey: $ISSUER_XKEY
+  }
+}
+
+system_account: SYS
+EOF
+
+# Start the server.
+nats-server -c server.conf > /dev/null 2>&1 &
+sleep 1
+
+# Write out some users emulating a user directory backend.
+cat <<- EOF > users.json
+{
+  "sys": {
+    "pass": "sys",
+    "account": "SYS"
+  },
+  "alice": {
+    "pass": "alice",
+    "account": "APP"
+  },
+  "bob": {
+    "pass": "bob",
+    "account": "APP",
+    "permissions": {
+      "pub": {
+        "allow": ["bob.>"]
+      },
+      "sub": {
+        "allow": ["bob.>"]
+      },
+      "resp": {
+        "max": 1
+      }
+    }
+  }
+}
+EOF
+
+# Start the auth callout service providing the auth credentials
+# to connect and the issuer seed.
+service \
+  -nats.url $NATS_URL \
+  -nats.user auth \
+  -nats.pass auth \
+  -issuer.seed $ISSUER_NSEED \
+  -xkey.seed $ISSUER_XSEED \
+  -users users.json &
+
+sleep 1
+
+# Confirm alice can connect and publish.
+echo 'Test alice'
+nats --user alice --password alice pub test 'hello from alice'
+
+# Confirm bob cannot publish to a subject they are not allowed to.
+echo 'Test bob on denied subject'
+nats --user bob --password bob pub test 'hello from bob' || true
+
+# Confirm bob can connect and publish to a subject they are allowed to.
+echo 'Test bob on allowed subject'
+nats --user bob --password bob pub bob.test 'hello from bob'
+
+# Confirm an unknown user cannot connect.
+echo 'Test unknown user'
+nats --user pam --password pam pub test 'hello from pam' || true
+
+# Confirm the system account user has the expected system permission.
+echo 'Test system account user'
+nats --user sys --password sys server list
---
+++ examples/auth/callout/cli/service/main.go
@@ -0,0 +1,200 @@
+package main
+
+import (
+	"encoding/json"
+	"flag"
+	"fmt"
+	"log"
+	"os"
+	"os/signal"
+
+	"github.com/nats-io/jwt/v2"
+	"github.com/nats-io/nats.go"
+	"github.com/nats-io/nkeys"
+)
+
+func main() {
+	if err := run(); err != nil {
+		fmt.Fprintln(os.Stderr, err)
+		os.Exit(1)
+	}
+}
+
+func run() error {
+	var (
+		natsUrl    string
+		natsUser   string
+		natsPass   string
+		issuerSeed string
+		xkeySeed   string
+		usersFile  string
+	)
+
+	flag.StringVar(&natsUrl, "nats.url", nats.DefaultURL, "NATS server URL")
+	flag.StringVar(&natsUser, "nats.user", "", "NATS user")
+	flag.StringVar(&natsPass, "nats.pass", "", "NATS password")
+	flag.StringVar(&issuerSeed, "issuer.seed", "", "Issuer seed")
+	flag.StringVar(&xkeySeed, "xkey.seed", "", "Xkey seed")
+	flag.StringVar(&usersFile, "users", "", "Users file")
+
+	flag.Parse()
+
+	// Parse the issuer account signing key.
+	issuerKeyPair, err := nkeys.FromSeed([]byte(issuerSeed))
+	if err != nil {
+		return fmt.Errorf("error parsing issuer seed: %s", err)
+	}
+
+	// Parse the xkey seed if present.
+	var curveKeyPair nkeys.KeyPair
+	if len(xkeySeed) > 0 {
+		curveKeyPair, err = nkeys.FromSeed([]byte(xkeySeed))
+		if err != nil {
+			return fmt.Errorf("error parsing xkey seed: %s", err)
+		}
+	}
+
+	// Model the user encoded in the users file.
+	type User struct {
+		Pass        string
+		Account     string
+		Permissions jwt.Permissions
+	}
+
+	// Load and decode the users file.
+	users := make(map[string]*User)
+	data, err := os.ReadFile(usersFile)
+	if err != nil {
+		return fmt.Errorf("error reading user directory: %s", err)
+	}
+	err = json.Unmarshal(data, &users)
+	if err != nil {
+		return fmt.Errorf("error parsing user directory: %s", err)
+	}
+
+	// Open the NATS connection passing the auth account creds file.
+	nc, err := nats.Connect(natsUrl, nats.UserInfo(natsUser, natsPass))
+	if err != nil {
+		return err
+	}
+	defer nc.Drain()
+
+	// Helper function to construct an authorization response.
+	respondMsg := func(msg *nats.Msg, userNkey, serverId, userJwt, errMsg string) {
+		rc := jwt.NewAuthorizationResponseClaims(userNkey)
+		rc.Audience = serverId
+		rc.Error = errMsg
+		rc.Jwt = userJwt
+
+		token, err := rc.Encode(issuerKeyPair)
+		if err != nil {
+			log.Printf("error encoding response JWT: %s", err)
+			msg.Respond(nil)
+			return
+		}
+
+		data := []byte(token)
+
+		// Check if encryption is required.
+		xkey := msg.Header.Get("Nats-Server-Xkey")
+		if len(xkey) > 0 {
+			data, err = curveKeyPair.Seal(data, xkey)
+			if err != nil {
+				log.Printf("error encrypting response JWT: %s", err)
+				msg.Respond(nil)
+				return
+			}
+		}
+
+		msg.Respond(data)
+	}
+
+	// Define the message handler for the authorization request.
+	msgHandler := func(msg *nats.Msg) {
+		var token []byte
+
+		// Check for Xkey header and decrypt
+		xkey := msg.Header.Get("Nats-Server-Xkey")
+		if len(xkey) > 0 {
+			if curveKeyPair == nil {
+				respondMsg(msg, "", "", "", "xkey not supported")
+				return
+			}
+
+			// Decrypt the message.
+			token, err = curveKeyPair.Open(msg.Data, xkey)
+			if err != nil {
+				respondMsg(msg, "", "", "", "error decrypting message")
+				return
+			}
+		} else {
+			token = msg.Data
+		}
+
+		// Decode the authorization request claims.
+		rc, err := jwt.DecodeAuthorizationRequestClaims(string(token))
+		if err != nil {
+			respondMsg(msg, "", "", "", err.Error())
+			return
+		}
+
+		// Used for creating the auth response.
+		userNkey := rc.UserNkey
+		serverId := rc.Server.ID
+
+		// Check if the user exists.
+		userProfile, ok := users[rc.ConnectOptions.Username]
+		if !ok {
+			respondMsg(msg, userNkey, serverId, "", "user not found")
+			return
+		}
+
+		// Check if the credential is valid.
+		if userProfile.Pass != rc.ConnectOptions.Password {
+			respondMsg(msg, userNkey, serverId, "", "invalid credentials")
+			return
+		}
+
+		// Prepare a user JWT.
+		uc := jwt.NewUserClaims(rc.UserNkey)
+		uc.Name = rc.ConnectOptions.Username
+
+		// Audience contains the account in non-operator mode.
+		uc.Audience = userProfile.Account
+
+		// Set the associated permissions if present.
+		uc.Permissions = userProfile.Permissions
+
+		// Validate the claims.
+		vr := jwt.CreateValidationResults()
+		uc.Validate(vr)
+		if len(vr.Errors()) > 0 {
+			respondMsg(msg, userNkey, serverId, "", "error validating claims")
+			return
+		}
+
+		// Sign it with the issuer key since this is non-operator mode.
+		ejwt, err := uc.Encode(issuerKeyPair)
+		if err != nil {
+			respondMsg(msg, userNkey, serverId, "", "error signing user JWT")
+			return
+		}
+
+		respondMsg(msg, userNkey, serverId, ejwt, "")
+	}
+
+	// Create a queue subscription to the auth callout subject. This
+	// allows for running multiple instances to distribute the load
+	// and provide high availability.
+	_, err = nc.QueueSubscribe("$SYS.REQ.USER.AUTH", "auth-callout", msgHandler)
+	if err != nil {
+		return err
+	}
+
+	// Block and wait for interrupt.
+	sigch := make(chan os.Signal, 1)
+	signal.Notify(sigch, os.Interrupt)
+	<-sigch
+
+	return nil
+}
