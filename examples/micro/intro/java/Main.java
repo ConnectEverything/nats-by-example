46c0eaf637474c183b1bf846e6da3c87eb6714a0
scottf <scottfauerbach@gmail.com>
2023-10-23 22:07:20 -0400

    fixed errors

--- examples/micro/intro/java/Main.java
+++ examples/micro/intro/java/Main.java
@@ -40,13 +40,13 @@ public class Main {
       ServiceEndpoint min = ServiceEndpoint.builder()
           .endpointName("min")
           .group(serviceGroup)
-          .handler(msg -> minRequestHandler(conn, msg))
+          .handler(msg -> minRequestHandler(nc, msg))
           .build();
 
       ServiceEndpoint max = ServiceEndpoint.builder()
           .endpointName("max")
           .group(serviceGroup)
-          .handler(msg -> maxRequestHandler(conn, msg))
+          .handler(msg -> maxRequestHandler(nc, msg))
           .build();
 
       // ### Defining the Service
@@ -61,7 +61,7 @@ public class Main {
           .description("Returns the min/max number in a request")
           .addServiceEndpoint(min)
           .addServiceEndpoint(max)
-          .connection(conn)
+          .connection(nc)
           .build();
 
       System.out.println("Created Service: " + service.getName() + " with the id: " + service.getId());
@@ -79,8 +79,8 @@ public class Main {
 
       // To "call" a service, we simply make a request to the proper endpoint with
       // data that it expects. Notice how the group name is prepended to the endpoint name.
-      CompletableFuture<Message> minResponse = conn.request("minmax.min", input);
-      CompletableFuture<Message> maxResponse = conn.request("minmax.max", input);
+      CompletableFuture<Message> minResponse = nc.request("minmax.min", input);
+      CompletableFuture<Message> maxResponse = nc.request("minmax.max", input);
 
       Message minMessage = minResponse.get(1, TimeUnit.SECONDS);
       System.out.println("Min value is: " + new String(minMessage.getData()));
@@ -103,5 +103,23 @@ public class Main {
       // * TimeoutException: the request took longer than the timeout specified
       System.err.println(e);
     }
+  }
+
+  static void minRequestHandler(Connection conn, ServiceMessage msg) {
+    int min = Integer.MAX_VALUE;
+    String[] input = new String(msg.getData()).split(",");
+    for (String n : input) {
+      min = Math.min(min, Integer.parseInt(n));
+    }
+    msg.respond(conn, ("" + min).getBytes());
+  }
+
+  static void maxRequestHandler(Connection conn, ServiceMessage msg) {
+    int max = Integer.MIN_VALUE;
+    String[] input = new String(msg.getData()).split(",");
+    for (String n : input) {
+      max = Math.max(max, Integer.parseInt(n));
+    }
+    msg.respond(conn, ("" + max).getBytes());
   }
 }
