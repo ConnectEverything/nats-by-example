7052aceceb3c29d116d2475289c7963e34bcec19
Ziya Suzen <ziya@suzen.net>
2023-11-26 04:48:48 +0000

    KV, Obj and services

--- examples/os/intro/dotnet2/Main.cs
+++ examples/os/intro/dotnet2/Main.cs
@@ -1,9 +1,12 @@
-// Install NuGet packages `NATS.Net`, `NATS.Client.Serializers.Json` and `Microsoft.Extensions.Logging.Console`.
+// Install NuGet packages `NATS.Net` and `Microsoft.Extensions.Logging.Console`.
 
-using System;
+using System.Net.WebSockets;
 using Microsoft.Extensions.Logging;
+using Microsoft.VisualBasic;
 using NATS.Client.Core;
-using NATS.Client.Serializers.Json;
+using NATS.Client.JetStream;
+using NATS.Client.ObjectStore;
+using NATS.Client.ObjectStore.Models;
 
 using var loggerFactory = LoggerFactory.Create(builder => builder.AddConsole());
 var logger = loggerFactory.CreateLogger("NATS-by-Example");
@@ -17,11 +20,77 @@ var opts = new NatsOpts
 {
     Url = url,
     LoggerFactory = loggerFactory,
-    SerializerRegistry = NatsJsonSerializerRegistry.Default,
     Name = "NATS-by-Example",
 };
 await using var nats = new NatsConnection(opts);
+var js = new NatsJSContext(nats);
+var obj = new NatsObjContext(js);
 
+// ### Object store basics
+// An object-store (OS) bucket is created by specifying a bucket name.
+// Here we try to access a store called "configs", if it doesn't exist
+// the API will create it:
+var store = await obj.CreateObjectStore("configs");
+
+// You can get information on the object store by getting its info:
+/*let status = await os.status();
+console.log(`the object store has ${status.size} bytes`);
+*/
+var status = await store.GetStatusAsync();
+logger.LogInformation("The object store has {Size} bytes", status.Info.State.Bytes);
+
+// 10MiB
+const int bytes = 10_000_000;
+var data = new byte[bytes];
+
+// Let's add an entry to the object store
+var info = await store.PutAsync(key: "a", data);
+logger.LogInformation("Added entry {Name} ({Size} bytes)- '{Description}'", info.Name, info.Size, info.Description);
+
+// Entries in an object store are made from a "metadata" that describes the object
+// And the payload. This allows you to store information about the significance of the
+// entry separate from the raw data.
+// You can update the metadata directly
+await store.UpdateMetaAsync("a", new ObjectMetadata { Name = "a", Description = "still large data" });
+
+// we expect this store to only contain one entry
+// You can list its contents:
+/*let entries = await os.list();
+console.log(`the object store contains ${entries.length} entries`);*/
+var count = 0;
+await foreach (var entry in store.ListAsync())
+{
+    logger.LogInformation("Entry {Name} ({Size} bytes)- '{Description}'", info.Name, info.Size, info.Description);
+    count++;
+}
+logger.LogInformation("The object store contains {Count} entries", count);
+
+// Now lets retrieve the item we added
+var data1 = await store.GetBytesAsync("a");
+logger.LogInformation("Data has {Size} bytes", data1.Length);
+
+// You can watch an object store for changes:
+var watcher = Task.Run(async () =>
+{
+    await foreach (var m in store.WatchAsync(new NatsObjWatchOpts{IncludeHistory = false}))
+    {
+        logger.LogInformation(">>>>>>>> Watch: {Bucket} changed '{Name}' {Op}", m.Bucket, m.Name, m.Deleted ? "was deleted" : "was updated");
+    }
+});
+
+// To delete an entry:
+await store.DeleteAsync("a");
+
+// Because the client may be working with large assets, ObjectStore
+// normally presents a "Stream" based API.
+info = await store.PutAsync(new ObjectMetadata { Name = "b", Description = "set with a stream" }, new MemoryStream(data));
+logger.LogInformation("Added entry {Name} ({Size} bytes)- '{Description}'", info.Name, info.Size, info.Description);
+
+var ms = new MemoryStream();
+info = await store.GetAsync("b", ms);
+logger.LogInformation("Got entry {Name} ({Size} bytes)- '{Description}'", info.Name, info.Size, info.Description);
+
+await obj.DeleteObjectStore("configs", CancellationToken.None);
 
 // That's it!
 logger.LogInformation("Bye!");
--- examples/services/intro/dotnet2/Main.cs
+++ examples/services/intro/dotnet2/Main.cs
@@ -4,6 +4,7 @@ using System;
 using Microsoft.Extensions.Logging;
 using NATS.Client.Core;
 using NATS.Client.Serializers.Json;
+using NATS.Client.Services;
 
 using var loggerFactory = LoggerFactory.Create(builder => builder.AddConsole());
 var logger = loggerFactory.CreateLogger("NATS-by-Example");
@@ -21,7 +22,72 @@ var opts = new NatsOpts
     Name = "NATS-by-Example",
 };
 await using var nats = new NatsConnection(opts);
+var svc = new NatsSvcContext(nats);
 
+// ### Defining a Service
+//
+// This will create a service definition. Service definitions are made up of
+// the service name (which can't have things like whitespace in it), a version,
+// and a description. Even with no running endpoints, this service is discoverable
+// via the micro protocol and by service discovery tools like `nats micro`.
+// All of the default background handlers for discovery, PING, and stats are
+// started at this point.
+var service = await svc.AddServiceAsync(new NatsSvcConfig("minmax", "0.0.1")
+{
+    Description = "Returns the min/max number in a request"
+});
+
+// Each time we create a service, it will be given a new unique identifier. If multiple
+// copies of the `minmax` service are running across a NATS subject space, then tools
+// like `nats micro` will consider them like unique instances of the one service and the
+// endpoint subscriptions are queue subscribed, so requests will only be sent to one
+// endpoint _instance_ at a time.
+// TODO: service.Info
+
+// ### Adding endpoints
+//
+// Groups serve as namespaces and are used as a subject prefix when endpoints
+// don't supply fixed subjects. In this case, all endpoints will be listening
+// on a subject that starts with `minmax.`
+var root = await service.AddGroupAsync("minmax");
+
+// Adds two endpoints to the service, one for the `min` operation and one for
+// the `max` operation. Each endpoint represents a subscription. The supplied handlers
+// will respond to `minmax.min` and `minmax.max`, respectively.
+await root.AddEndpointAsync(HandleMin, "min", serializer: NatsJsonSerializer<int[]>.Default);
+await root.AddEndpointAsync(HandleMax, "max", serializer: NatsJsonSerializer<int[]>.Default);
+
+// Make a request of the `min` endpoint of the `minmax` service, within the `minmax` group.
+// Note that there's nothing special about this request, it's just a regular NATS
+// request.
+var min = await nats.RequestAsync<int[], int>("minmax.min", new[]
+{
+    -1, 2, 100, -2000
+}, requestSerializer: NatsJsonSerializer<int[]>.Default);
+logger.LogInformation("Requested min value, got {Min}", min.Data);
+
+// Make a request of the `max` endpoint of the `minmax` service, within the `minmax` group.
+var max = await nats.RequestAsync<int[], int>("minmax.max", new[]
+{
+    -1, 2, 100, -2000
+}, requestSerializer: NatsJsonSerializer<int[]>.Default);
+logger.LogInformation("Requested max value, got {Max}", max.Data);
+
+// The statistics being managed by micro should now reflect the call made
+// to each endpoint, and we didn't have to write any code to manage that.
+// TODO: service.Stats
 
 // That's it!
 logger.LogInformation("Bye!");
+
+ValueTask HandleMin(NatsSvcMsg<int[]> msg)
+{
+    var min = msg.Data.Min();
+    return msg.ReplyAsync(min);
+}
+
+ValueTask HandleMax(NatsSvcMsg<int[]> msg)
+{
+    var min = msg.Data.Max();
+    return msg.ReplyAsync(min);
+}
