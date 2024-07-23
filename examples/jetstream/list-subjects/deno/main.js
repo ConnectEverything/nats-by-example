669630c830068ecf0d4290851903b38a9775081f
scottf <scottfauerbach@gmail.com>
2024-07-23 15:58:19 -0400

    random stream and subject names

--- examples/jetstream/list-subjects/deno/main.js
+++ examples/jetstream/list-subjects/deno/main.js
@@ -1,6 +1,6 @@
 import {
-    StorageType,
-    connect
+    connect,
+    nuid
 } from "https://deno.land/x/nats@v1.28.0/src/mod.ts";
 
 const servers = Deno.env.get("NATS_URL")?.split(",");
@@ -10,40 +10,40 @@ const nc = await connect({ servers });
 const jsm = await nc.jetstreamManager();
 const js = nc.jetstream();
 
-// Create a stream
-// (remove the stream first so we have a clean starting point)
-try {
-    await jsm.streams.delete("subjects");
-} catch (err) {
-    if (err.code != 404) {
-        console.error(err.message);
-    }
-}
+const randomName: () => string = function (): string {
+    return nuid.next().substring(18)
+};
 
-// Create a stream with a few subjects
+// Create a stream with a few random subjects
+const plainSubj = `PLAIN_${randomName()}`;
+const gtSubj = `GT_${randomName()}`;
+const starSubj = `STAR_${randomName()}`;
+const name = `EVENTS_${randomName()}`;
+
 let si = await jsm.streams.add({
-    name: "subjects",
-    subjects: ["plain", "greater.>", "star.*"],
-    storage: StorageType.Memory,
+    name,
+    subjects: [plainSubj, `${gtSubj}.>`, `${starSubj}.*`]
 });
+
+console.log(`Stream: ${si.config.name} has these subjects: '${si.config.subjects}'`)
 
 // ### Get stream info with StreamInfoRequestOptions
 // Get the subjects via the streams.info call.
 // Since this is "state" there are no subjects in the state unless
 // there are messages in the subject.
-si = await jsm.streams.info("subjects", {subjects_filter: ">"});
+si = await jsm.streams.info(name, {subjects_filter: ">"});
 
 const count = si.state.subjects
     ? Object.getOwnPropertyNames(si.state.subjects).length
     : 0;
-console.log(`Before publishing any messages, there are 0 subjects: ${count}`)
+console.log(`Before publishing any messages, there should be 0 subjects in the state: ${count}`)
 
 // console.log(`Before publishing any messages, there are 0 subjects: ${si.state.num_subjects}`)
 
 // Publish a message
-await js.publish("plain")
+await js.publish(plainSubj)
 
-si = await jsm.streams.info("subjects", {subjects_filter: ">"});
+si = await jsm.streams.info(name, {subjects_filter: ">"});
 console.log("After publishing a message to a subject, it appears in state:")
 
 if (si.state.subjects) {
@@ -55,18 +55,18 @@ if (si.state.subjects) {
 }
 
 // Publish some more messages, this time against wildcard subjects
-await js.publish("greater.A", "gtA-1");
-await js.publish("greater.A", "gtA-2");
-await js.publish("greater.A.B", "gtAB-1");
-await js.publish("greater.A.B", "gtAB-2");
-await js.publish("greater.A.B.C", "gtABC");
-await js.publish("greater.B.B.B", "gtBBB");
-await js.publish("star.1", "star1-1");
-await js.publish("star.1", "star1-2");
-await js.publish("star.2", "star2");
+await js.publish(`${gtSubj}.A`, "gtA-1");
+await js.publish(`${gtSubj}.A`, "gtA-2");
+await js.publish(`${gtSubj}.A.B`, "gtAB-1");
+await js.publish(`${gtSubj}.A.B`, "gtAB-2");
+await js.publish(`${gtSubj}.A.B.C`, "gtABC");
+await js.publish(`${gtSubj}.B.B.B`, "gtBBB");
+await js.publish(`${starSubj}.1`, "star1-1");
+await js.publish(`${starSubj}.1`, "star1-2");
+await js.publish(`${starSubj}.2`, "star2");
 
 // Get all subjects
-si = await jsm.streams.info("subjects", {subjects_filter: ">"});
+si = await jsm.streams.info(name, {subjects_filter: ">"});
 console.log("Wildcard subjects show the actual subject, not the template.");
 
 if (si.state.subjects) {
@@ -79,8 +79,8 @@ if (si.state.subjects) {
 
 // ### Subject Filtering
 // Instead of allSubjects, you can filter for a specific subject
-si = await jsm.streams.info("subjects", {subjects_filter: "greater.>"});
-console.log("Filtering the subject returns only matching entries ['greater.>']");
+si = await jsm.streams.info(name, {subjects_filter: `${gtSubj}.>`});
+console.log(`Filtering the subject returns only matching entries ['${gtSubj}.>']`);
 
 if (si.state.subjects) {
     let subjects = {};
@@ -90,8 +90,8 @@ if (si.state.subjects) {
     }
 }
 
-si = await jsm.streams.info("subjects", {subjects_filter: "greater.A.>"});
-console.log("Filtering the subject returns only matching entries ['greater.A.>']");
+si = await jsm.streams.info(name, {subjects_filter: `${gtSubj}.A.>`});
+console.log(`Filtering the subject returns only matching entries ['${gtSubj}.A>']`);
 
 if (si.state.subjects) {
     let subjects = {};
